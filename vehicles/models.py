d88ee73ed94d9dfd444b4fa0f1fb3165a7c80874
cxt314 <103610756+cxt314@users.noreply.github.com>
2025-05-16 18:07:43 +0000

    initial vehicle model

--- vehicles/models.py
+++ vehicles/models.py
@@ -1,3 +1,30 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
+class FuelType(models.Model):
+    name = models.CharField(max_length=50)
+    desc = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Vehicle(models.Model):
+    name = models.CharField(max_length=200)
+    year = models.PositiveIntegerField(
+        validators=[MinValueValidator(1900), MaxValueValidator(2100)]
+    )
+    make = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    fuel_type = models.ForeignKey(FuelType, on_delete=models.CASCADE)
+    purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
+    vin = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
