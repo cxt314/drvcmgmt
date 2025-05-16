2dde0c75f63ba8845ae8fd0a818a15c557eb266a
cxt314 <103610756+cxt314@users.noreply.github.com>
2025-05-16 18:22:00 +0000

    allow blank vin

--- vehicles/models.py
+++ vehicles/models.py
@@ -37,7 +37,7 @@ class Vehicle(models.Model):
         default=HYBRID,
     )
     purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
-    vin = models.CharField(max_length=20)
+    vin = models.CharField(max_length=20, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
